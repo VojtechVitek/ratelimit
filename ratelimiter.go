c14bbf38a5fdfe9ef620cd10892ce9d41c9a625a
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2016-05-08 18:54:29 -0400

    Add X-RateLimit response headers

--- ratelimiter.go
+++ ratelimiter.go
@@ -1,20 +1,24 @@
 package ratelimiter
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
 type KeyFn func(r *http.Request) string
 
-func Middleware(keyFn KeyFn, rate time.Duration, burstLimit int) func(http.Handler) http.Handler {
-	l := inMemoryRateLimiter{
-		rate:       rate,
-		keyFn:      keyFn,
-		burstLimit: burstLimit,
-		buckets:    map[string]chan token{},
+func Middleware(keyFn KeyFn, rate time.Duration, burst int) func(http.Handler) http.Handler {
+	if burst < 1 {
+		burst = 1
 	}
-	l.Run()
+	l := inMemoryRateLimiter{
+		rate:    rate,
+		keyFn:   keyFn,
+		burst:   burst,
+		buckets: map[string]chan token{},
+	}
+	go l.Run()
 
 	fn := func(h http.Handler) http.Handler {
 		l.next = h
@@ -26,26 +30,24 @@ func Middleware(keyFn KeyFn, rate time.Duration, burstLimit int) func(http.Handl
 type token struct{}
 
 type inMemoryRateLimiter struct {
-	next       http.Handler
-	keyFn      KeyFn
-	rate       time.Duration
-	burstLimit int
-	buckets    map[string]chan token
+	next    http.Handler
+	keyFn   KeyFn
+	rate    time.Duration
+	burst   int
+	buckets map[string]chan token
 }
 
 func (l *inMemoryRateLimiter) Run() {
-	go func() {
-		tick := time.NewTicker(l.rate)
-		for range tick.C {
-			for key, bucket := range l.buckets {
-				select {
-				case <-bucket:
-				default:
-					delete(l.buckets, key)
-				}
+	tick := time.NewTicker(l.rate)
+	for range tick.C {
+		for key, bucket := range l.buckets {
+			select {
+			case <-bucket:
+			default:
+				delete(l.buckets, key)
 			}
 		}
-	}()
+	}
 }
 
 // ServeHTTPC implements http.Handler interface.
@@ -53,11 +55,15 @@ func (l *inMemoryRateLimiter) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 	key := l.keyFn(r)
 	bucket, ok := l.buckets[key]
 	if !ok {
-		bucket = make(chan token, l.burstLimit)
+		bucket = make(chan token, l.burst)
 		l.buckets[key] = bucket
 	}
 	select {
 	case bucket <- token{}:
+		w.Header().Set("X-RateLimit-Key", fmt.Sprintf("%v", key))
+		w.Header().Set("X-RateLimit-Rate", fmt.Sprintf("%v req/min", int(time.Minute/l.rate)))
+		w.Header().Set("X-RateLimit-Limit", fmt.Sprintf("%v", cap(bucket)))
+		w.Header().Set("X-RateLimit-Remaining", fmt.Sprintf("%v", cap(bucket)-len(bucket)))
 		l.next.ServeHTTP(w, r)
 	default:
 		http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
